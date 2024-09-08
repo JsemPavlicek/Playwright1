f2e57bfd960f58bede677b2ee7a560e90bea69df
Pavel Formánek <jsempavlicek@seznam.cz>
2024-09-08 07:16:12 +0200

    created new file get_attribute.py

---
+++ get_attribute.py
@@ -0,0 +1,19 @@
+from playwright.sync_api import sync_playwright
+
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto('https://demo.automationtesting.in/Selectable.html')
+    cookies = page.locator('p.fc-button-label:has-text("Consent")')
+
+    if cookies.count() > 0:
+        cookies.first.click()
+    a_elements = page.locator('a')
+    print(a_elements.count())
+    for i in range(a_elements.count()):
+        one_element = a_elements.nth(i)
+        one_element_attribute = one_element.get_attribute('href')
+        print(one_element_attribute)
+    
+    browser.close()
